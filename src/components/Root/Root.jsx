149577e0de441d95acedde09125bdea92ee5ffaa
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-06-22 11:25:05 +0300

    Выводит на экран простой React компонент

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,6 @@
+// Core
+import React from "react";
+
+export const Root = () => {
+  return <div>The root react component</div>;
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Core
+import React from "react";
+import {render} from "react-dom";
+
+// Other
+import {Root} from "./components";
+
+// App
+render(<Root />, document.getElementById(`root`));
