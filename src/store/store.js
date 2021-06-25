8842d94e0fe6243d7305d1913d7dd5d5b314b5bb
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-06-25 12:39:40 +0300

    Подключает Redux Devtools Extension

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
 // Core
 import {createStore} from "redux";
 
+// Dev Tool
+import {composeWithDevTools} from "redux-devtools-extension/developmentOnly";
+
 // Root reducer
 import rootReducer from "./root-reducer";
 
 // Store
-export default createStore(rootReducer);
+export default createStore(rootReducer, composeWithDevTools());
