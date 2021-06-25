72f28b93fcddf9e74ab0631c876b9ef2f716e303
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-06-25 13:28:48 +0300

    Подключает Redux Logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 // Core
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 
 // Dev Tool
 import {composeWithDevTools} from "redux-devtools-extension/developmentOnly";
@@ -7,5 +7,15 @@ import {composeWithDevTools} from "redux-devtools-extension/developmentOnly";
 // Root reducer
 import rootReducer from "./root-reducer";
 
+// Middlewares
+const middlewares = [];
+import {logger} from "./middlewares/redux-logger";
+
+// TODO: Нужно убрать с pruduction версии
+middlewares.push(logger);
+
 // Store
-export default createStore(rootReducer, composeWithDevTools());
+export default createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(...middlewares))
+);
