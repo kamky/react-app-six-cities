f6d0c3df688373fd0431a31b6d5a26cf7a480e46
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-06-25 14:08:25 +0300

    Создает переменные окружения Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,46 @@
 /* global __dirname */
+
+// Core
 const path = require(`path`);
 
-module.exports = {
-  devtool: `source-map`,
-  entry: `./src/index.js`,
-  output: {
-    filename: `bundle.js`,
-    path: path.join(__dirname, `public`),
-  },
-  devServer: {
-    port: 1337,
-    open: true,
-    compress: false,
-    historyApiFallback: true,
-    contentBase: path.join(__dirname, `public`),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: `babel-loader`,
+// Plugins
+const {DefinePlugin} = require(`webpack`);
+
+// Webpack config function
+module.exports = (_env, argv) => {
+  return {
+    devtool: `source-map`,
+    entry: `./src/index.js`,
+    output: {
+      filename: `bundle.js`,
+      path: path.join(__dirname, `public`),
+    },
+    devServer: {
+      port: 1337,
+      open: true,
+      compress: false,
+      historyApiFallback: true,
+      contentBase: path.join(__dirname, `public`),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: `babel-loader`,
+          },
         },
-      },
+      ],
+    },
+    resolve: {
+      extensions: [`.js`, `.jsx`],
+    },
+    plugins: [
+      new DefinePlugin({
+        __DEV__: JSON.stringify(argv.mode === `development`),
+        __PROD__: JSON.stringify(argv.mode === `production`),
+      }),
     ],
-  },
-  resolve: {
-    extensions: [`.js`, `.jsx`],
-  },
+  };
 };
