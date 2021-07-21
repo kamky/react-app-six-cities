53aaaa768e4f3d65d322ad71c52b2ac6401eec60
Dmitriy Kuznetsov <dmitriy@kuznetsov.com>
2021-07-21 10:17:36 +0300

    Создает статическую страницу авторизации

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,13 +3,28 @@ import React from "react";
 
 // Components
 import {Header} from "../../components";
+import {LoginForm} from "../../components";
 
 // Component
 export const Login = () => {
   return (
     <div className="page page--gray page--login">
       <Header />
-      <div>Login Page</div>
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">Sign in</h1>
+            <LoginForm />
+          </section>
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#">
+                <span>Amsterdam</span>
+              </a>
+            </div>
+          </section>
+        </div>
+      </main>
     </div>
   );
 };
